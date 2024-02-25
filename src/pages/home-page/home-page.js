ab3e43b2190d4f9ebae617344be098d366025c06
Artem Kozatskyi <artemfury@gmail.com>
2024-02-25 13:15:00 +0200

    feature: Implemented the ability for admins to add, edit, and delete products

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,10 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ItemCard from "../../components/itemCard/itemCard";
 import "./home-page.css";
-import api from '../../api';
+import api from "../../api";
+import CustomInput from "../../components/customInput/customInput";
 
 function HomePage() {
+  const role = localStorage.getItem("role");
+  const accessToken = localStorage.getItem("accessToken");
   const [items, setItems] = useState([]);
+  const [search, setSearch] = useState("");
+  const [creating, setCreating] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -18,23 +26,94 @@ function HomePage() {
 
     fetchItems();
   }, []);
+  const handleSearch = async () => {
+    try {
+      const response = await api.get(`/items?search=${search}`);
+      setItems(response.data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await api.post(
+        `/items`,
+        {
+          name,
+          description,
+          price,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setCreating(false);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  return (
-    <>
-      <h1>Перелік товарів</h1>
-      <div className="item-list">
-        {items.map((item, index) => (
-          <ItemCard
-            key={index}
-            name={item.name}
-            price={item.price}
-            id={item.id}
-            description={item.description}
+  if (creating) {
+    return (
+      <form onSubmit={handleSubmit} className="creating-item">
+        <CustomInput
+          label={"Назва"}
+          placeholder={"Назва"}
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <CustomInput
+          label={"Опис"}
+          placeholder={"Опис"}
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <CustomInput
+          label={"Ціна"}
+          placeholder={"Ціна"}
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type="submit">Зберегти</button>
+      </form>
+    );
+  } else {
+    return (
+      <>
+        <h1>Перелік товарів</h1>
+        <div className="search">
+          <CustomInput
+            type="search"
+            placeholder={"Пошук"}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
-        ))}
-      </div>
-    </>
-  );
+          <button onClick={handleSearch}>Пошук</button>
+          {role === "admin" && (
+            <buton onClick={() => setCreating(true)}>Створити товар</buton>
+          )}
+        </div>
+        <div className="item-list">
+          {items.map((item, index) => (
+            <ItemCard
+              key={index}
+              name={item.name}
+              price={item.price}
+              id={item.id}
+              description={item.description}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default HomePage;
--- src/pages/item-page/item-page.js
+++ src/pages/item-page/item-page.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./item-page.css";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../../api";
+import CustomInput from "../../components/customInput/customInput";
 
 function ItemPage() {
+  const role = localStorage.getItem("role");
+  const accessToken = localStorage.getItem("accessToken");
   let { itemId } = useParams();
-  const [item, setItem] = useState({}); 
+  const [item, setItem] = useState({});
+  const [editing, setEditing] = useState(false);
   const [quantity, setQuantity] = useState(1);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+
   const navigate = useNavigate();
   useEffect(() => {
     const fetchItem = async () => {
       try {
         const response = await api.get(`/items/${itemId}`);
         setItem(response.data);
+        setName(response.data.name);
+        setDescription(response.data.description);
+        setPrice(response.data.price);
       } catch (error) {
         console.error("Error fetching items:", error);
       }
@@ -35,36 +46,111 @@ function ItemPage() {
     const items = JSON.parse(localStorage.getItem("cartItems")) || [];
     items.push(newItem);
     localStorage.setItem("cartItems", JSON.stringify(items));
-    navigate('/cart')
+    navigate("/cart");
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await api.put(
+        `/items/${itemId}`,
+        {
+          name,
+          description,
+          price,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setEditing(false);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleDeleteItem = async () => {
+    try {
+      await api.delete(`/items/${itemId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      navigate("/");
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
   };
 
   return (
-    <div className="item-page">
-      <div className="item-details">
-        <h1>{item.name}</h1>
-        <img
-          src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c08183651.png"
-          alt={item.name}
-          className="item-image"
-        />
-        <p className="item-price">${item.price}</p>
-        <p className="description">{item.description}</p>
-      </div>
-      <div className="quantity-input">
-        <label htmlFor="quantity">Кількість:</label>
-        <input
-          type="number"
-          id="quantity"
-          name="quantity"
-          value={quantity}
-          onChange={handleQuantityChange}
-          min="1"
-        />
-        <button className="add-to-cart-btn" onClick={handleAddToCart}>
-          Додати у кошик
-        </button>
-      </div>
-    </div>
+    <>
+      {editing ? (
+        <form onSubmit={handleSubmit} className="editing-item">
+          <CustomInput
+            label={"Назва"}
+            placeholder={"Назва"}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <CustomInput
+            label={"Опис"}
+            placeholder={"Опис"}
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <CustomInput
+            label={"Ціна"}
+            placeholder={"Ціна"}
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <button type="submit">Зберегти</button>
+        </form>
+      ) : (
+        <>
+          {role === "admin" && (
+            <div className="buttons">
+              <button className="edit-btn" onClick={() => setEditing(true)}>
+                Редагувати
+              </button>
+              <button className="delete-btn" onClick={handleDeleteItem}>
+                Видалити
+              </button>
+            </div>
+          )}
+          <div className="item-page">
+            <div className="item-details">
+              <h1>{item.name}</h1>
+              <img
+                src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c08183651.png"
+                alt={item.name}
+                className="item-image"
+              />
+              <p className="item-price">${item.price}</p>
+              <p className="description">{item.description}</p>
+            </div>
+            <div className="quantity-input">
+              <label htmlFor="quantity">Кількість:</label>
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                value={quantity}
+                onChange={handleQuantityChange}
+                min="1"
+              />
+              <button className="add-to-cart-btn" onClick={handleAddToCart}>
+                Додати у кошик
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
